6a70c15bd36f887f8cf32e01eceed3071b37b672
Alex Wilson <fonzmp@gmail.com>
2018-05-17 00:28:51 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,12 +6,12 @@ class School
 
   def initialize(name)
     @name = name
-    @roster = {}
+    @@roster = {}
   end
 
   def add_student(student_name)
     @student = student_name
-    @roster << student_name
+    @@roster << student_name
   end
 
 end
