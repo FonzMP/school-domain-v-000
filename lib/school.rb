ac51a6b4e2b9cb1d5a3e623c39792e7804b43208
Alex Wilson <fonzmp@gmail.com>
2018-05-17 00:48:59 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,6 +12,10 @@ class School
   def add_student=(student_name, grade)
     @student_name = student_name
     @grade = grade
+
+  end
+
+  def add_student
     School.roster.key << grade
     School.roster.value << student_name
   end
