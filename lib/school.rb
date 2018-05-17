6b5f9f51dae4e2f4e79389a0c5e6f2a5d7627a64
Alex Wilson <fonzmp@gmail.com>
2018-05-17 00:43:49 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,15 +3,14 @@
 class School
 
   attr_accessor :roster
-  attr_writer :add_student
 
   def initialize(name)
     @name = name
     @roster = {}
   end
 
-  def add_student(student_name, school)
-    
+  def add_student=(student_name, school)
+
   end
 
 end
